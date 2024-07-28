4137038fa1ab6228abdd5e72c1b6e809c0238f3a
SKA <133320803+ike-shiro@users.noreply.github.com>
2024-07-28 15:16:10 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let parent = document.getElementById('bg_lines');
 let elem = parent.querySelector('.bg_line');
 
 
-for (let i = 0; i < 200; i++) {
+for (let i = 0; i < 170; i++) {
     let clone = elem.cloneNode(true);
     parent.appendChild(clone);
 }
